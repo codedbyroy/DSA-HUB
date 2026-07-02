d7b82725b09716f1d88dc8b852519b9637cc68fd
Ankush Roy <ankushroy.dev@gmail.com>
2026-07-02 16:18:06 +0530

    Update Arrays solutions to match my exact accepted LeetCode implementations

--- Arrays/LC1920_BuildArrayFromPermutation.java
+++ Arrays/LC1920_BuildArrayFromPermutation.java
@@ -1,14 +1,8 @@
-// LC 1920 - Build Array from Permutation
-// https://leetcode.com/problems/build-array-from-permutation/
-// Difficulty: Easy
-// Time: O(n) | Space: O(n)
-
 class LC1920_BuildArrayFromPermutation {
     public int[] buildArray(int[] nums) {
-        int[] ans = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
+        int ans[]=new int[nums.length];
+        for( int i = 0;i < nums.length;i++)
             ans[i] = nums[nums[i]];
-        }
         return ans;
     }
 }
--- Arrays/LC1929_ConcatenationOfArray.java
+++ Arrays/LC1929_ConcatenationOfArray.java
@@ -1,15 +1,9 @@
-// LC 1929 - Concatenation of Array
-// https://leetcode.com/problems/concatenation-of-array/
-// Difficulty: Easy
-// Time: O(n) | Space: O(n)
-
 class LC1929_ConcatenationOfArray {
     public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] ans = new int[2 * n];
-        for (int i = 0; i < n; i++) {
+        int ans[]=new int[2*nums.length];
+        for(int i = 0;i < nums.length;i++){
             ans[i] = nums[i];
-            ans[i + n] = nums[i];
+            ans[i +nums.length] = ans[i];
         }
         return ans;
     }
